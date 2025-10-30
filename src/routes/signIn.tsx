b5cdb62d5b0553fe731bcd6c7e39774407cf5e01
Mox Shah <mox@evincedev.com>
2025-10-30 18:32:15 +0530

    Remove unstructured files

--- src/routes/signIn.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/signIn")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Sign In</div>;
-}
