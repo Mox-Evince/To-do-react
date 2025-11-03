feeaad4166c791397b1f1f4d4ccde5375a653159
Mox Shah <mox@evincedev.com>
2025-11-03 11:30:24 +0530

    wrap App in AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,59 @@
+import { createContext, useCallback, useMemo, useState } from "react";
+
+export interface AuthContextType {
+  isAuthenticated: boolean;
+  login: (username: string) => void;
+  logout: () => void;
+  user: string | null;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+const loggedInKey = "loggedIn";
+
+const getLoggedinInfo = () => {
+  const loggedIn = localStorage.getItem(loggedInKey);
+  return loggedIn;
+};
+
+const setLoggedInInfo = (value: string) => {
+  localStorage.setItem(loggedInKey, value);
+};
+
+const removeLoggedInInfo = () => {
+  localStorage.removeItem(loggedInKey);
+};
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<string | null>(getLoggedinInfo());
+
+  const login = useCallback((username: string) => {
+    if (username) {
+      setLoggedInInfo(username);
+      setUser(username);
+    }
+  }, []);
+
+  const logout = useCallback(() => {
+    removeLoggedInInfo();
+    setUser(null);
+  }, []);
+
+  const authContextValue: AuthContextType = useMemo(
+    () => ({
+      isAuthenticated: false, // Replace with actual authentication state
+      login,
+      user,
+      logout,
+    }),
+    [login, logout, user]
+  );
+
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,41 +1,8 @@
-import {
-  createRootRoute,
-  Link,
-  Navigate,
-  Outlet,
-} from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { Route as signInRoute } from "./signIn";
 
 const RootLayout = () => (
   <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{" "}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>{" "}
-      <Link to={signInRoute.to} className="[&.active]:font-bold">
-        SignIn
-      </Link>{" "}
-      <Link
-        to="/user/$id"
-        params={{ id: "123" }}
-        search={{
-          showAll: "true",
-        }}
-        // hash="mox-123"
-        className="[&.active]:font-bold"
-      >
-        User
-      </Link>{" "}
-      <Link to="/post/{-$id}" className="[&.active]:font-bold">
-        Post
-      </Link>{" "}
-      <Navigate to="/about"></Navigate>
-    </div>
-    <hr />
     <Outlet />
     <TanStackRouterDevtools />
   </>
